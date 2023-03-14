f7f30e93b5a9421c2c29228b13d808004e57b6ee
Yinuo Deng <i@dyn.im>
2023-03-14 14:59:04 +0800

    Added configurable features

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 pub mod proxy;
 mod error;
 mod target;
+
+#[cfg(feature = "enable_sync")]
 pub mod sync;
+
+#[cfg(feature = "enable_async_std")]
 pub mod async_std;
+
+#[cfg(feature = "enable_tokio")]
 pub mod tokio;
 mod utils;
 mod prelude;
